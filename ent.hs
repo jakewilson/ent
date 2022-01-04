7343c214a2d80254b593c519a3b95454b712e1e0
Jake Wilson <jakewilsonfl@protonmail.com>
2022-01-04 15:55:51 -0500

    Add (^) function.
    Fix operator order bug.

--- ent.hs
+++ ent.hs
@@ -2,21 +2,35 @@ module Eint where
 
 import Control.Applicative((<|>))
 import Data.Char
-import System.IO
+import Prelude hiding (exp)
+import System.IO (hFlush, stdout)
 import Text.ParserCombinators.ReadP
 
 getOp :: Char -> (Int -> Int -> Int)
-getOp '^' = (^)
-getOp '-' = (-)
-getOp '/' = div
-getOp '*' = (*)
-getOp _   = (+)
+getOp = flip . getOp'
+  where
+    getOp' '^' = (^)
+    getOp' '-' = (-)
+    getOp' '/' = div
+    getOp' '*' = (*)
+    getOp' _   = (+)
 
 spaces :: ReadP String
 spaces = many (satisfy (== ' '))
 
 factor :: ReadP Int
 factor = read <$> (spaces *> many1 (satisfy isDigit) <* spaces)
+
+expRight :: ReadP (Int -> Int)
+expRight = do
+  op <- char '^'
+  getOp op <$> exp
+
+exp :: ReadP Int
+exp = do
+  n <- factor
+  f <- option (+ 0) expRight
+  return $ f n
 
 termRight :: ReadP (Int -> Int)
 termRight = do
@@ -25,7 +39,7 @@ termRight = do
 
 term :: ReadP Int
 term = do
-  n1 <- factor
+  n1 <- exp
   f <- option (+ 0) termRight
   return (f n1)
 
@@ -44,10 +58,6 @@ parseExpr :: String -> Maybe Int
 parseExpr input = if null result then Nothing else Just $ fst $ last result
   where
     result = readP_to_S expr input
-
-printResult :: Maybe Int -> IO ()
-printResult Nothing  = putStrLn "Enter a valid number"
-printResult (Just x) = print x
 
 main :: IO ()
 main = do
